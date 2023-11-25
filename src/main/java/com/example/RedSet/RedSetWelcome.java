d11092774de583854aa81064c663f08aa67af439
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2023-11-25 15:59:38 +0600

    Welcome page updated!

--- src/main/java/com/example/RedSet/RedSetWelcome.java
+++ src/main/java/com/example/RedSet/RedSetWelcome.java
@@ -1,14 +1,20 @@
 package com.example.RedSet;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,9 +28,39 @@ public class RedSetWelcome implements Initializable {
 
     @FXML
     private AnchorPane anchorPane;
+    @FXML
+    private Label loadingPercentage;
+
+    @FXML
+    private ProgressBar loadingbar;
+
+    @FXML
+    private Circle outer;
+
+    int val = 0;
+
+    private void setContinuousRotate(Circle c, int angle, double duration) {
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(duration), event -> {
+            c.setRotate(c.getRotate() + angle);
+            if(val <= 95) {
+                loadingbar.setProgress(loadingbar.getProgress() + 0.0032);
+                val = (int) (loadingbar.getProgress() * 100);
+                loadingPercentage.setText(Integer.toString(val) + " %");
+            }
+        });
+
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadingbar.setProgress(0.0000);
+        loadingPercentage.setText("0 %");
+        setContinuousRotate(outer, 1, 0.010);
         splash();
     }
 
