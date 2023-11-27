b479cb645117a0bbcb471bdc623dd6c47b5cfd00
tamal267 <syedtamal@gmail.com>
2023-11-27 19:27:03 +0600

    updated anchorpane

--- src/main/java/com/example/RedSet/Transition.java
+++ src/main/java/com/example/RedSet/Transition.java
@@ -1,4 +1,11 @@
 package com.example.RedSet;
 
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
 public class Transition {
+    public static void loadscene(String fxml, Stage currentStage, Parent Rootpane)
+    {
+
+    }
 }
